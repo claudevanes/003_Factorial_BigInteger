57b2e010459ca2c3b28ef38c89c3cc8abf2befae
claudevanes <dvorskakamila@gmail.com>
2016-05-12 09:12:31 +0200

    first commit

---
+++ src/sas/Main.java
@@ -0,0 +1,16 @@
+package sas;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("003");
+		
+	}
+
+}
